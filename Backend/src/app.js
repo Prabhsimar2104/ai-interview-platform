b36104c2f529d419acc4714ea375ca544b10d195
Prabhsimar2104 <simarprabh095@gmail.com>
2026-07-01 18:37:12 +0530

    CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,21 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ai-interview-platform-one-amber.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
-}))
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
